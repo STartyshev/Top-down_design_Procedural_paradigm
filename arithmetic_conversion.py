f99672807d04ddfcf5a014d30e15ac5774e2fa3a
Semen Tartyshev <s.tartyshev@yandex.ru>
2023-10-27 23:55:21 +0700

    Добавлена реализация алгоритма третьей задачи.

--- arithmetic_conversion.py
+++ arithmetic_conversion.py
@@ -124,13 +124,68 @@ def arithmetic_conversion(first_array, second_array, third_array):
     :param first_array: Первый массив чисел;
     :param second_array: второй массив чисел;
     :param third_array: третий массив чисел, который будет проверяться.
+    :return: Список со строками в которых будет содержаться описание результатов проверки
+    (можно или нельзя преобразовать).
     """
-    pass
+    list_of_results = ['' for i in range(len(third_array))]
+    triples_of_numbers = enumerate(zip(first_array, second_array, third_array))
+    for index, triple in triples_of_numbers:
+        try:
+            check_arithmetic_operation(
+                index, triple[0], triple[1], triple[2], list_of_results,
+                lambda elem1, elem2: elem1 % elem2, '%'
+            )
+            check_arithmetic_operation(
+                index, triple[0], triple[1], triple[2], list_of_results,
+                lambda elem1, elem2: elem1 / elem2, '/'
+            )
+            check_arithmetic_operation(
+                index, triple[0], triple[1], triple[2], list_of_results,
+                lambda elem1, elem2: elem1 ** elem2, '**'
+            )
+            check_arithmetic_operation(
+                index, triple[0], triple[1], triple[2], list_of_results,
+                lambda elem1, elem2: elem1 * elem2, '*'
+            )
+            check_arithmetic_operation(
+                index, triple[0], triple[1], triple[2], list_of_results,
+                lambda elem1, elem2: elem1 - elem2, '-'
+            )
+            check_arithmetic_operation(
+                index, triple[0], triple[1], triple[2], list_of_results,
+                lambda elem1, elem2: elem1 + elem2, '+'
+            )
+        except ZeroDivisionError:
+            pass
+
+    for i in range(len(list_of_results)):
+        if list_of_results[i] == '':
+            list_of_results[i] = f"Нет способов получить {i + 1}-й элемент."
+
+    return list_of_results
 
 
-def check_arithmetic_operation():
+def check_arithmetic_operation(index, elem1, elem2, elem3, list_of_results, func, function_symbol):
     """
     Функция реализующая проверку: можно ли получить число из 3-го массива, заданным арифметическим преобразованием
     с числами 2-ух других массивов. В случае положительного результата добавляет полученый способ в список результатов.
+    :param index: Индекс проверяемого элемента;
+    :param elem1: элемент из первого массива;
+    :param elem2: элемент из второго массива;
+    :param elem3: элемент из третьего массива (проверяемый элемент);
+    :param list_of_results: список полученных результатов;
+    :param func: функция которая будет применяться к elem1 и elem2 (арифметическая операция);
+    :param function_symbol: строковое отображение арифметической операции (значок).
     """
-    pass
+    if func(elem1, elem2) == elem3:
+        if list_of_results[index] == '':
+            list_of_results[index] = (f"Способы получить {index + 1}-й элемент: "
+                                      f"{elem1} {function_symbol} {elem2} = {elem3};")
+        else:
+            list_of_results[index] += f"\n{elem1} {function_symbol} {elem2} = {elem3};"
+    elif func(elem2, elem1) == elem3:
+        if list_of_results[index] == '':
+            list_of_results[index] = (f"Способы получить {index + 1}-й элемент: "
+                                      f"{elem2} {function_symbol} {elem1} = {elem3};")
+        else:
+            list_of_results[index] += f"\n{elem2} {function_symbol} {elem1} = {elem3};"
