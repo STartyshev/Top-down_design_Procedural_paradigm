4651aaa4b6e25fbdd758c58d6019943daf89a867
Semen Tartyshev <s.tartyshev@yandex.ru>
2023-10-28 00:05:53 +0700

    Добавлена реализация модуля error_output.

--- error_output.py
+++ error_output.py
@@ -3,4 +3,7 @@ def print_error_message(message):
     Функция реализующая форматированный вывод сообщения об ошибке в консоль.
     :param message: Текст сообщения об ошибке.
     """
-    pass
+    system('CLS')
+    print(message)
+    system('PAUSE')
+    system('CLS')
