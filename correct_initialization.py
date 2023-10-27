cd34d235ffc74087c2d269cafae6eac952d5b6cf
Semen Tartyshev <s.tartyshev@yandex.ru>
2023-10-28 00:02:50 +0700

    Добавлена реализация части модуля correct_initialization.

--- correct_initialization.py
+++ correct_initialization.py
@@ -1,3 +1,6 @@
+import random
+
+
 def input_correct_num(left_value, right_value, input_message, error_message, type_of_number):
     """
     Функция реализующая ввод пользователем корректного числа входящего в заданный диапазон.
@@ -6,8 +9,21 @@ def input_correct_num(left_value, right_value, input_message, error_message, typ
     :param input_message: сообщение-приглашение на ввод числа;
     :param error_message: сообщение в случае не корректного ввода;
     :param type_of_number: тип числа, которое нужно вводить пользователю.
+    :return: Возвращает корректное число введенное пользователем.
     """
-    pass
+    while True:
+        while True:
+            try:
+                num = type_of_number(input(input_message))
+                break
+            except ValueError:
+                print('Введенное значение должно являться числом! '
+                      'Попробуйте еще раз.')
+
+        if num < left_value or num > right_value:
+            print(error_message)
+        else:
+            return num
 
 
 def ui_array_init(array):
@@ -15,7 +31,17 @@ def ui_array_init(array):
     Функция для инициализации массива числами вручную.
     :param array: Массив для инициализации.
     """
-    pass
+    while True:
+        elem_or_exit = input('Введите число, которое хотите добавить в массив (для выхода введите - выход): ')
+        if elem_or_exit.upper() == 'ВЫХОД':
+            return
+        else:
+            try:
+                elem_or_exit = float(elem_or_exit)
+                array.append(elem_or_exit)
+            except ValueError:
+                print_error_message('Введенное значение не является числом или ключевым словом "выход". '
+                                    'Попробуйте еще раз.')
 
 
 def auto_array_init(array):
@@ -23,7 +49,16 @@ def auto_array_init(array):
     Функция для инициализации массива случайными числами.
     :param array: Массив для инициализации.
     """
-    pass
+    n = input_correct_num(
+        left_value=1,
+        right_value=50,
+        input_message='Введите размер массива: ',
+        error_message='Размер массива должен принимать целочисленное значение и быть больше нуля '
+                      'и меньше или равен 50. Попробуйте еще раз.',
+        type_of_number=int
+    )
+    for i in range(n):
+        array.append(round(random.random() * 10 * ((-1) ** random.randint(1, 2)), 1))
 
 
 def ui_array_of_dots_init(array_of_dots):
