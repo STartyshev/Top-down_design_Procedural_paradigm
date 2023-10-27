f5ad967b5ed26b2776dafecbb0a1de67047e3a23
Semen Tartyshev <s.tartyshev@yandex.ru>
2023-10-27 23:41:33 +0700

    Добавлена реализация UI для второй задачи и описание входных параметров вспомогательных функций.

--- correct_initialization.py
+++ correct_initialization.py
@@ -1,6 +1,11 @@
-def input_correct_num():
+def input_correct_num(left_value, right_value, input_message, error_message, type_of_number):
     """
     Функция реализующая ввод пользователем корректного числа входящего в заданный диапазон.
+    :param left_value: Левая граница диапазона;
+    :param right_value: правая граница диапазона;
+    :param input_message: сообщение-приглашение на ввод числа;
+    :param error_message: сообщение в случае не корректного ввода;
+    :param type_of_number: тип числа, которое нужно вводить пользователю.
     """
     pass
 
@@ -21,15 +26,18 @@ def auto_array_init(array):
     pass
 
 
-def ui_array_of_dots_init():
+def ui_array_of_dots_init(array_of_dots):
     """
     Функция реализующая пользовательский интерфейс в консоли для инициализации массива точек вручную.
+    :param array_of_dots: Массив точек для инициализации.
     """
     pass
 
 
-def ui_auto_array_of_dots_init():
+def ui_auto_array_of_dots_init(array_of_dots, space_selection):
     """
     Функция реализующая пользовательский интерфейс в консоли для инициализации массива случайными точками.
+    :param array_of_dots: Массив точек для инициализации;
+    :param space_selection: размерность пространства в котором будут находиться точки.
     """
     pass
